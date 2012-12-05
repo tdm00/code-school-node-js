00c203a73776a3845bee9af80a2b9a4b8fdf1734
Troy Murray <troy.murray@gmail.com>
2012-12-05 13:38:40 -0500

    Level 1, challenge 7 complete

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 var http = require('http');
-var fs = require('fs');
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {
-    'Content-Type': 'text/html'});
-
-  fs.readFile('index.html', function(err, contents) {
-    response.write(contents);
-    response.end();
-  });
-
+  response.writeHead(200);
+  response.end("Hello, this is dog");
 }).listen(8080);
 
